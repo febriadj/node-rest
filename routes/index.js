e2884118985fb15c6fa9526c0731289bbe6f314b
i'mfebri <iamfebriadji@gmail.com>
2021-04-13 15:37:34 +0700

    menambahkan routing untuk post products

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const { getProducts } = require('../controllers/getProducts')
+const { postProducts } = require('../controllers/postProducts')
 
 const routes = async function(req, res) {
   if (req.url == '/products' && req.method == 'GET') {
-    await getProducts(req, res) // include getProducts controllers
+    const execution = await getProducts(req, res) // include getProducts controllers
+    return execution
+  }
+
+  if (req.url == '/products' && req.method == 'POST') {
+    const execution = await postProducts(req, res)
+    return execution
   }
 }
 
